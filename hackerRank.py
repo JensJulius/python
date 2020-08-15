fb0e2a19cbe5bc76572ed768d90df43f453a39b8
JensJulius <julriksen@gmail.com>
2020-08-15 10:28:53 +0200

    Daily update

--- hackerRank.py
+++ hackerRank.py
@@ -1,2 +1,26 @@
-print("Test fra desk")
+if __name__ == '__main__':
+	n = int(input())
+	arr = map(int, input().split())
 
+	s_arr = sorted(arr, reverse=True)
+	print(s_arr)
+
+	for i in s_arr:
+		if i == s_arr[0]:
+			continue
+		else:
+			print(i)
+			break
+	# for i in arr:
+	# 	if i > largest_digit:
+	# 		second_largest_digit = largest_digit
+	# 		largest_digit = i
+
+	# 	elif i > second_largest_digit and i < largest_digit:
+	# 		smallest_digit = second_largest_digit
+	# 		second_largest_digit = i
+
+	# 	else:
+	# 		smallest_digit = i
+
+	# print(second_largest_digit)
--- mastermind.py
+++ mastermind.py
@@ -3,9 +3,19 @@
 
 #	Code maker creates code (Three - six  (four) spots, Three - six  (four) colors.
 #	- More than one of each color can be used
+targetList = []
+
+def createSequence(s1, s2, s3, s4):
+	targetList.append(s1, s2, s3, s4)
+
+
+createSequence("red", "blue", "green", "blue")
+print(targetList)
 
 #	Code breaker makes first guess
-#
+
+# def
+
 #	Code maker gives hints to correctness.
 #	- White peg for correct color, but wrong spot
 #	- Black peg for correct color correct spot
